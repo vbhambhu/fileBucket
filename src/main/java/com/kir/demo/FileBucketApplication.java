0270db36ee2f68f139d740cb8b3a99d9716647ea
Vinod Kumar <vkumar@kirmac86.kennedy.ox.ac.uk>
2017-09-22 16:24:12 +0100

    Initial commit

---
+++ src/main/java/com/kir/demo/FileBucketApplication.java
@@ -0,0 +1,12 @@
+package com.kir.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileBucketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kir/demo/FileBucketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kir.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileBucketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
